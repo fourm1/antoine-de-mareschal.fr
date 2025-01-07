2416866eca6386eb7b853c577e5fc70f135f9711
fourm1 <toto.63146@gmail.com>
2025-01-07 16:58:19 +0100

    Customizing users class

--- back/user/models.py
+++ back/user/models.py
@@ -1,3 +1,11 @@
+from django.contrib.auth.models import AbstractUser
 from django.db import models
 
 # Create your models here.
+
+class CustomUser(AbstractUser):
+    last_name = models.CharField(null=True, blank=True),
+    first_name = models.CharField(null=True, blank=True),
+    created_at = models.DateField(),
+    last_login = models.DateField(),
+    profile_picture = models.IntegerField(),
